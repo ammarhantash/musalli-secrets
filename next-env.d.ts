a41c962ed282034a933fcf8c1ab28a685dcd1318
ammarhantash <31186811+ammarhantash@users.noreply.github.com>
2026-06-23 07:17:51 +0300

    chore: update gitignore and sync next-env.d.ts for v0.1

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file is auto-managed by Next.js. Do not edit.
-// It will be regenerated on `npm run dev` / `npm run build`.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
